cf21890882aef589f65beb924dd18273f120ec66
rglennp44 <rglennp@gmail.com>
2020-12-12 21:28:08 -0700

    Upload Code to Version Control
    Initial upload

---
+++ puzzlein.py
@@ -0,0 +1,27 @@
+def testPuzzle():
+    # data = [
+    #     [5, 3, None, None, 7, None, None, None, None],
+    #     [6, None, None, 1, 9, 5, None, None, None],
+    #     [None, 9, 8, None, None, None, None, 6, None],
+    #     [8, None, None, None, 6, None, None, None, 3],
+    #     [4, None, None, 8, None, 3, None, None, 1],
+    #     [7, None, None, None, 2, None, None, None, 6],
+    #     [None, 6, None, None, None, None, 2, 8, None],
+    #     [None, None, None, 4, 1, 9, None, None, 5],
+    #     [None, None, None, None, 8, None, None, 7, 9],
+    # ]
+
+    # data format: array of columns
+
+    data = [
+        [5, 6, None, 8, 4, 7, None, None, None],
+        [3, None, 9, None, None, None, 6, None, None],
+        [None, None, 8, None, None, None, None, None, None],
+        [None, 1, None, None, 8, None, None, 4, None],
+        [7, 9, None, 6, None, 2, None, 1, 8],
+        [None, 5, None, None, 3, None, None, 9, None],
+        [None, None, None, None, None, None, 2, None, None],
+        [None, None, 6, None, None, None, 8, None, 7],
+        [None, None, None, 3, 1, 6, None, 5, 9],
+    ]
+    return data
